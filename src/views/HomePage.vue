40bfa62f22be46a3df55fb2e9087313156597dd8
Juan C Martinez <stc.classcontent@gmail.com>
2024-02-25 16:28:35 -0600

    added style to categories

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -14,7 +14,13 @@ onMounted(async () => {
 
 <template>
   <BaseTitle>TRIVIA APPLICATION</BaseTitle>
-  <div v-for ="category in categories" :key="category.id">
-    {{ category.name }}
+  <div class="grid flex-grow grid-cols-4 gap-12 m-20">
+    <RouterLink v-for ="category in categories" :key="category.id"
+    :to="``"
+    class="bg-white text-center flex h-32 items-center justify-center rounded-lg border-4 border-slate-500 py-4 font-bold uppercase hover:cursor-pointer hover:bg-blue-400 hover:text-white transition-colors duration-350"
+    >
+      {{ category.name }}
+    </RouterLink>
   </div>
+  
 </template>
